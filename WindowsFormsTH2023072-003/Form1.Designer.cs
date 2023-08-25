116a1de12ef4753ff0ee4bf3b3640072247c7f7f
PavelSadykov <2439747@gmail.com>
2023-08-25 11:20:01 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsTH2023072-003/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsTH2023072_003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.thicknessTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(45, 51);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(283, 314);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(417, 51);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(179, 23);
+            this.loadImageButton.TabIndex = 1;
+            this.loadImageButton.Text = "loadImageButton";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click_1);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(417, 97);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(179, 23);
+            this.colorButton.TabIndex = 2;
+            this.colorButton.Text = "colorButton";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click_1);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(417, 200);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(179, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "saveButton";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click_1);
+            // 
+            // thicknessTrackBar
+            // 
+            this.thicknessTrackBar.Location = new System.Drawing.Point(417, 137);
+            this.thicknessTrackBar.Name = "thicknessTrackBar";
+            this.thicknessTrackBar.Size = new System.Drawing.Size(179, 45);
+            this.thicknessTrackBar.TabIndex = 4;
+            this.thicknessTrackBar.Scroll += new System.EventHandler(this.thicknessTrackBar_Scroll_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.thicknessTrackBar);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.loadImageButton);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TrackBar thicknessTrackBar;
+    }
+}
+
---
+++ WindowsFormsTH2023072-003/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTH2023072_003
+{
+    public partial class Form1 : Form
+    {
+        private Image loadedImage;
+        private Bitmap drawingBitmap;
+        private Graphics drawingGraphics;
+        private Pen currentPen;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            drawingBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            drawingGraphics = Graphics.FromImage(drawingBitmap);
+            currentPen = new Pen(Color.Black, 2);
+
+            drawingGraphics.Clear(Color.White);
+            pictureBox.Image = drawingBitmap;
+        }
+
+        private void loadImageButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                loadedImage = Image.FromFile(openFileDialog.FileName);
+                drawingGraphics.DrawImage(loadedImage, Point.Empty);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void colorButton_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPen.Color = colorDialog.Color;
+            }
+        }
+
+        private void thicknessTrackBar_Scroll_1(object sender, EventArgs e)
+        {
+            currentPen.Width = thicknessTrackBar.Value;
+
+            MessageBox.Show($"Pen thickness set to: {currentPen.Width}");
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show($"Mouse Down at X: {e.X}, Y: {e.Y}");
+                drawingGraphics.DrawLine(currentPen, e.X, e.Y, e.X, e.Y);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image|*.png";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingBitmap.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
